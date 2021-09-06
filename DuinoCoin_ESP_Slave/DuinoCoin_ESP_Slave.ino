d2955b3b68646265ebc93f5449791aed0aa73c2b
JK-Rolling <89238608+JK-Rolling@users.noreply.github.com>
2021-09-06 21:01:32 +0800

    Update DuinoCoin_ESP_Slave.ino

--- DuinoCoin_ESP_Slave/DuinoCoin_ESP_Slave.ino
+++ DuinoCoin_ESP_Slave/DuinoCoin_ESP_Slave.ino
@@ -5,15 +5,52 @@
   
   Modified by JK-Rolling
 */
+#include <Ticker.h>
+
+// Loop WDT... please don't feed me...
+// See lwdtcb() and lwdtFeed() below
+Ticker lwdTimer;
+#define LWD_TIMEOUT   60000
+
+unsigned long lwdCurrentMillis = 0;
+unsigned long lwdTimeOutMillis = LWD_TIMEOUT;
+
+void RestartESP(String msg) {
+  Serial.println(msg);
+  Serial.println("Resetting ESP...");
+  Blink();
+  ESP.reset();
+}
+
+// Our new WDT to help prevent freezes
+// code concept taken from https://sigmdel.ca/michel/program/esp8266/arduino/watchdogs2_en.html
+void ICACHE_RAM_ATTR lwdtcb(void)
+{
+  if ((millis() - lwdCurrentMillis > LWD_TIMEOUT) || (lwdTimeOutMillis - lwdCurrentMillis != LWD_TIMEOUT))
+    RestartESP("Loop WDT Failed!");
+}
+
+void lwdtFeed(void) {
+  lwdCurrentMillis = millis();
+  lwdTimeOutMillis = lwdCurrentMillis + LWD_TIMEOUT;
+}
+
 
 void setup() {
   Serial.begin(115200);
   DuinoCoin_setup();
+
+  lwdtFeed();
+  lwdTimer.attach_ms(LWD_TIMEOUT, lwdtcb);
+  
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, LOW);
 }
 
 void loop() {
+  // 1 minute watchdog
+  lwdtFeed();
+  
   if (DuinoCoin_loop())
   {
     Serial.print(F("Job Done : "));
