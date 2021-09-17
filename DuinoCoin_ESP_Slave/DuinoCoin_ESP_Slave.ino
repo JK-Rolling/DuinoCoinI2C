c4c0ed65dbdd6930111a48e055743103fc932efe
JK-Rolling <89238608+JK-Rolling@users.noreply.github.com>
2021-09-17 17:14:25 +0800

    Remove LED blink as it takes away CPU time

--- DuinoCoin_ESP_Slave/DuinoCoin_ESP_Slave.ino
+++ DuinoCoin_ESP_Slave/DuinoCoin_ESP_Slave.ino
@@ -9,23 +9,6 @@
 // uncomment this line to use ESP-01
 //#define ESP01
 
-#ifndef ESP01
-#define LED LED_BUILTIN
-#endif
-
-#ifdef LED
-#define LedBegin()                pinMode(LED, OUTPUT);
-#define LedBlink()                digitalWrite(LED, state ^= HIGH); delay(50);
-#else
-#define LedBegin()
-#define LedBlink()
-#endif
-
-#define BLINK_SHARE_FOUND    1
-#define BLINK_SETUP_COMPLETE 2
-#define BLINK_CLIENT_CONNECT 3
-#define BLINK_RESET_DEVICE   5
-
 #include <Ticker.h>
 
 Ticker lwdTimer;
@@ -34,18 +17,9 @@ Ticker lwdTimer;
 unsigned long lwdCurrentMillis = 0;
 unsigned long lwdTimeOutMillis = LWD_TIMEOUT;
 
-void Blink(uint8_t count) {
-  uint8_t state = HIGH;
-
-  for (int x = 0; x < (count << 1); ++x) {
-    LedBlink();
-  }
-}
-
 void RestartESP(String msg) {
   Serial.println(msg);
   Serial.println("Resetting ESP...");
-  Blink(BLINK_RESET_DEVICE);
   #if ESP8266
     ESP.reset();
   #endif
@@ -85,8 +59,6 @@ void setup() {
   lwdtFeed();
   lwdTimer.attach_ms(LWD_TIMEOUT, lwdtcb);
   
-  LedBegin();
-  Blink(BLINK_SETUP_COMPLETE);
 }
 
 void loop() {
@@ -97,6 +69,5 @@ void loop() {
   {
     Serial.print(F("Job Done : "));
     Serial.print(DuinoCoin_response());
-    Blink(BLINK_SHARE_FOUND);
   }
 }
